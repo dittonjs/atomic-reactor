8d198ceba43ebaf67d2006a9e67a9a8ab6d382bb
dittonjs <jditton.atomic@gmail.com>
2018-11-21 21:17:29 -0700

    comma

--- webpack.config.js
+++ webpack.config.js
@@ -164,7 +164,7 @@ module.exports = function webpackConfig(app, options = {}) {
     { test: /.*\.(eot|woff2|woff|ttf)$/, use: ['url-loader?limit=500&hash=sha512&digest=hex&size=16&name=[name]-[hash].[ext]'] },
     { test: /\.tpl$/, loader: 'lodash-template-webpack-loader' },
     { test: /\.(graphql|gql)$/, exclude: /node_modules/, loader: 'graphql-tag/loader' },
-    { test: /\.csv$/, loader: 'csv-loader', options: { dynamicTyping: true, header: true, skipEmptyLines: true},
+    { test: /\.csv$/, loader: 'csv-loader', options: { dynamicTyping: true, header: true, skipEmptyLines: true}
   ];
 
   const entryPath = path.join(app.path, app.file);
